38aaf0f4fc498e4fa941ffaa6affd614dcc4cab9
Paul H. Liu <paul@thev.net>
2015-08-22 23:32:57 -0700

    Add a QuickCheck test case for assignment 7

--- program/prog-07.hs
+++ program/prog-07.hs
@@ -5,6 +5,8 @@
 
 import Data.Char
 import Control.Applicative hiding (many, (<|>))
+import Test.QuickCheck
+import Data.List (intersect)
 
 -- Datatype definition for lambda expression.
 data Term = Var Var | Lam Var Term | App Term Term deriving Eq
@@ -100,6 +102,69 @@ lbd = (sym '\\' <|> sym 'λ')
 sym = char . (==)
 
 space = many1 (sym ' ')
+ 
+-- Randomly generate Var and Term for QuickCheck.
+instance Arbitrary Var where
+  -- use a limited range to increase chances for variable re-use
+  arbitrary = fmap (V . (:[])) $ choose ('a', 'z')
+
+instance Arbitrary Term where
+  arbitrary = sized term
+    where
+      term 0 = Var <$> arbitrary
+      term n = oneof [ Var <$> arbitrary,
+                       Lam <$> arbitrary <*> term (n-1),
+                       App <$> term m <*> term m ]
+        where m = n `div` 2
+
+-- Testing for alphaEq's behavior
+propAlpha :: Int -> Int -> Term -> Bool
+propAlpha i j e = nlam == 0 || (testEq e e1 && testEq e1 e) &&
+                    (null fvs || testNEq e1 e2 && testNEq e2 e1 &&
+                                 testNEq e1 e3 && testNEq e3 e1) &&
+                    (null fvs' || testNEq e4 e4' && testNEq e4' e4 &&
+                                  testEq e5 e5' && testEq e5' e5)
+  where
+    testEq e e' = alphaEq e e' || error ("Expect True, but alphaEq (" ++ show e ++ ") (" ++ show e' ++ ") is False")
+    testNEq e e' = not (alphaEq e e') || error ("Expect False, but alphaEq (" ++ show e ++ ") (" ++ show e' ++ ") is True")
+    -- count the number of lambdas
+    nlam = fold (\_ -> 0) (\_ _ -> 1) (+) e 
+    n = abs i `mod` nlam
+    navigate i f e@(Lam v e') = if i == n then Left (e, f) else navigate (i + 1) (f . Lam v) e'
+    navigate i f e@(App e1 e2) = 
+      case navigate i (f . flip App e2) e1 of
+        Right i' -> navigate i' (f . App e1) e2
+        r -> r
+    navigate i f _ = Right i
+    Left (Lam v e', ctx) = navigate 0 id e
+    bvs = boundVars e'
+    fvs = filter (/=v) $ freeVars e'
+    -- a case that is always valid
+    u1 = head [ u | u <- allVars, u `notElem` bvs, u `notElem` fvs ]
+    e1 = ctx $ Lam u1 (subV v u1 e')
+    -- a case that is always invalid
+    u2 = fvs !! (abs j `mod` length fvs)
+    e2 = ctx $ Lam u2 (subV v u2 e')
+    -- a case that is always invalid
+    u3 = fvs !! (abs j `mod` length fvs)
+    e3 = ctx $ Lam v (subV u3 v e')
+    -- a case that is always invalid
+    fvs' = fvs `intersect` freeVars e
+    u4 = fvs' !! (abs j `mod` length fvs')
+    e4  = Lam u4 e
+    e4' = Lam v $ ctx $ Lam v (subV u4 v e')
+    -- a case that is always valid
+    (u5:u6:_) = [ u | u <- allVars, u `notElem` freeVars e, u `notElem` boundVars e, u `notElem` fvs, v `notElem` fvs ]
+    e5  = Lam u4 e
+    e5' = Lam u5 $ subV u4 u5 $ ctx $ Lam u6 (subV v u6 e')
+
+-- An infinite supply of variable names
+atoz = ['a' .. 'z']
+allVars = map V (map (:[]) atoz ++ [ v : show m | v <- atoz, m <- [0..]])
+
+-- Bounded variable set
+boundVars :: Term -> [Var]
+boundVars = fold (\_ -> []) (:) (++) 
 
 -- Free variable set
 freeVars :: Term -> [Var]
@@ -119,3 +184,14 @@ subV u w (Lam v e) | v == u    = Lam v e
                    | otherwise = Lam v (subV u w e)
 subV u w (App f e) = App (subV u w f) 
                          (subV u w e)
+ 
+
+-- ===========================================================
+-- You may modify the function below to complete the assignment.
+-- ===========================================================
+
+alphaEq :: Term -> Term -> Bool
+alphaEq e e' = True 
+
+-- You may test the correctness of your implementation with the following:
+-- main = quickCheck propAlpha
